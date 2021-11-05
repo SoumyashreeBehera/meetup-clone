529df250c866ea0198cb644d7c673f4c4f851e82
Mahipal Singh Haritwal <mahi98jat@gmail.com>
2021-11-05 14:05:35 +0530

    Blue print for events and groups added

---
+++ frontend/src/groupsandevents/Group.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Group() {
+    return (
+        <div>
+            I am from groups
+        </div>
+    )
+}
---
+++ frontend/src/groupsandevents/GroupAndEvent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Group from "./Group";
+import Events from "./Events";
+export default function GroupAndEvent() {
+  return (
+    <>
+      <Group />
+      <Events />
+    </>
+  );
+}
